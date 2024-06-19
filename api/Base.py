2e7c37718e90ceaef22241ccf58ff08547c6885b
fu-jw <contact@fu-jw.com>
2024-06-19 23:51:11 +0800

    路由规划，分为API路由和视图路由

--- api/Base.py
+++ api/Base.py
@@ -5,6 +5,7 @@
 @Des: 基本路由
 """
 from fastapi import APIRouter
+from api.login import login
 
 apiRouter = APIRouter(prefix="/v1", tags=["api路由"])
 
@@ -12,3 +13,12 @@ apiRouter = APIRouter(prefix="/v1", tags=["api路由"])
 @apiRouter.get('/')
 async def index():
     return "RUOK"
+
+
+"""
+上下两种路由写法：
+1.Python装饰器写法，
+2.路由调用法，路由和处理函数分离【推荐】
+"""
+apiRouter.get('/login')(login)
+# apiRouter.get('/login', tags=["api路由"], summary="登录接口")(login)
---
+++ api/login.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+@Created on : 2024/6/18 22:02
+@Author: Fredo
+@Des: 登录路由
+"""
+
+
+# 登录测试
+async def login():
+    return "login successfully"
